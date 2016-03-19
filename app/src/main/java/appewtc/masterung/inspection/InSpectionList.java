dc30c65857365fb07643ba6d8c89362f1d8a3888
masterUNG <phrombutr@gmail.com>
2016-03-19 18:02:20 +0700

    Finish 19March16

--- app/src/main/java/appewtc/masterung/inspection/InSpectionList.java
+++ app/src/main/java/appewtc/masterung/inspection/InSpectionList.java
@@ -2,12 +2,30 @@ package appewtc.masterung.inspection;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class InSpectionList extends AppCompatActivity {
+
+    //Explicit
+    private ImageView iconImageView;
+    private TextView titleTextView;
+    private ListView inspectorListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_spection_list);
+
+        //Bind Widget
+        bindWidget();
+
+    }   // Main Method
+
+    private void bindWidget() {
+
     }
-}
+
+}   // Main Class
