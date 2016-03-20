4f8b38833f8e6c93074f8586abac2e87d6e21fd8
masterUNG <phrombutr@gmail.com>
2016-03-20 17:06:53 +0700

    Create Cursor Where from Category finish

--- app/src/main/java/appewtc/masterung/inspection/InSpectionList.java
+++ app/src/main/java/appewtc/masterung/inspection/InSpectionList.java
@@ -1,7 +1,10 @@
 package appewtc.masterung.inspection;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -27,7 +30,25 @@ public class InSpectionList extends AppCompatActivity {
         //Show View
         showView();
 
+        //Create ListView
+        createListView();
+
     }   // Main Method
+
+    private void createListView() {
+
+        //Read Data From SQLite
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM " +
+                MyManage.inspection_table + " WHERE " +
+                MyManage.column_Category + " = " + "'" + chooseCategoryString + "'", null);
+
+        cursor.moveToFirst();
+        int intCount = cursor.getCount();
+        Log.d("Spect", "intCount ==> " + intCount);
+
+    }   // createListView
 
     private void showView() {
 
